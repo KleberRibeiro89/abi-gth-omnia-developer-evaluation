54027b0bec7e3970e3815ccb611075c1b5b4a692
Kleber Ribeiro <kleber.ribeiro89@gmail.com>
2025-02-10 13:16:05 -0300

    mapeando as entidades para vendas

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Entities/Sale.cs
@@ -0,0 +1,19 @@
+﻿using Ambev.DeveloperEvaluation.Common.Security;
+using Ambev.DeveloperEvaluation.Domain.Common;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities;
+
+public class Sale : BaseEntity
+{
+    public string Id { get; set; } = string.Empty;
+
+    public int SaleNumber { get; set; }
+
+    public string Customer { get; set; } = string.Empty;
+
+    public decimal TotalSaleAmount { get; set; }
+
+    public string BranchSaleMade { get; set; } = string.Empty;
+
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+}
