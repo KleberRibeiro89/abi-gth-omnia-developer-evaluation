2475563b11b3cbc83f12ee190cc556699e8b034d
Kleber Ribeiro <kleber.ribeiro89@gmail.com>
2025-02-10 23:05:54 -0300

    criando os handlers e mapeamento de produto

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Product/CreateProduct/CreateProductCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace Ambev.DeveloperEvaluation.Application.Product.CreateProduct;
+
+public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+{
+    public CreateProductCommandValidator()
+    {
+        RuleFor(p => p.Description)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Product Description is Required");
+
+        RuleFor(p => p.Price)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Price é required");
+    }
+}
--- template/backend/src/Ambev.DeveloperEvaluation.ORM/Mapping/ProductConfiguration.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/Mapping/ProductConfiguration.cs
@@ -18,7 +18,7 @@ public class ProductConfiguration : IEntityTypeConfiguration<Product>
         builder.HasKey(p => p.Id);
         builder.Property(p => p.Id).HasColumnType("uuid").HasDefaultValueSql("gen_random_uuid()");
 
-        builder.Property(p => p.Name).IsRequired().HasMaxLength(64);
+        builder.Property(p => p.Description).IsRequired().HasMaxLength(64);
         builder.Property(p => p.Price).IsRequired();
     }
 }
