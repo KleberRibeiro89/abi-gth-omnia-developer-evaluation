e03b67398567f9c62a4869acddec568de71a2553
Kleber Ribeiro <kleber.ribeiro89@gmail.com>
2025-02-12 20:03:51 -0300

    validacao

--- template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/CreateSale/CreateSaleCommandValidator.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/CreateSale/CreateSaleCommandValidator.cs
@@ -6,6 +6,19 @@ public class CreateSaleCommandValidator : AbstractValidator<CreateSaleCommand>
 {
     public CreateSaleCommandValidator()
     {
-        
+        RuleFor(s => s.CustomerID)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Customer id is required");
+
+        RuleFor(s => s.BranchSaleMade)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Branch sale made is required");
+
+        RuleFor(s => s.Items)
+            .NotEmpty()
+            .WithMessage("item is required");
+
     }
 }
